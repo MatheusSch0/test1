a6a37a3df66ec6af26cdee6ce598cefa4863a130
MatheusSch0 <144277969+MatheusSch0@users.noreply.github.com>
2023-09-13 16:51:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#exer 1
+
+n1 = int (input('fale um numero? '))
+n2 = int (input('me diz outro '))
+
+print('o resultado sera', n1 + n2)
+
+# Crie um sistema que imprima seu nome na tela
+
+nome = 'matheus'
+print(nome)
+
+# CRIE UM SISTEMA QUE IMPRIMA SEU NOME E UM CUMPRIMENTO APÓS nome(CONCATENE)
+desk = "uau"
+altura = input("e qual a sua altura? ")
+print(desk+altura)
+
+#CRIE UM SISTEMA QUE MULTIPLIQUE 2 NÚMEROS
+
+nu1 = int (input('mande um numero? '))
+nu2 = int (input('fale outro:'))
+
+print('o resultado é:',nu1 * nu2)
+     
+
+
+
+
+
+
